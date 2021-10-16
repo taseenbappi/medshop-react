f5fc32b104393f412c1998d54044194f2d81e8df
taseenbappi <taseen.bappi@gmail.com>
2021-10-17 01:41:03 +0600

    initial project & header and banner img added

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className="banner container-fluid">
+            <h1>Hello Banner</h1>
+        </div >
+    );
+};
+
+export default Banner;
---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../../images/Banner&Background/logo.png';
+
+const Header = () => {
+    return (
+        <div className="container-fluid m-auto">
+            <div className="menu-container row shadow-sm  ">
+                <div className="menu-img col-md-2 col-lg-2 col-12 col-sm-12 p-2">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="col-md-10 col-lg-10 col-12 col-sm-12" >
+                    <ul className=" items-container d-flex justify-content-lg-end p-2 justify-content-sm-center justify-content-md-center
+                justify-content-center">
+                        <li>Home</li>
+                        <li>Shop</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                    </ul>
+                </div>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default Header;
